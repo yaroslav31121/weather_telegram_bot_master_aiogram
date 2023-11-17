7da4e30d2a939b7705223e103679b608850fc100
yaroslav31121 <yaroslav31121@gmail.com>
2023-11-17 19:21:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-open_weather_token = "da376f8af0c35a9b06883b8947047436"
-
-tg_bot_token = "6564876060:AAFust-fo4ApXuXpE3vVcqySOtMJPJ0rHFk"
+open_weather_token = "openwether_token"
+tg_bot_token = "your_bot_token"   
